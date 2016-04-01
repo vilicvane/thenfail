f006ba37fb64a82e8a3260527c9ae90292b5f5cb
vilicvane <i@vilic.info>
2016-04-02 01:24:27 +0800

    Clean up index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,6 @@ export * from './errors';
 export * from './utils/lock';
 export * from './utils/promise-lock';
 
-import { Promise } from './promise';
-
-const { invoke, using } = Promise;
-
 export {
     Promise as default,
     invoke,
